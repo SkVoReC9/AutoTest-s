246c6ce68745d530576b3e4a30deb303a6a318da
Your Name <sashaskvorcov19@gmail.com>
2020-11-10 17:11:45 +0300

    Unittests for Web Clien

--- Site_Tests/Test_Login_and_Purch.py
+++ Site_Tests/Test_Login_and_Purch.py
@@ -1,8 +1,38 @@
 from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 import datetime
+from selenium.common.exceptions import TimeoutException
+import unittest
 
-driver = webdriver.Chrome()
-driver.get()
+
+class TestLogin(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+
+    def test_log_in_discord(self):
+        driver = self.driver
+        driver.get('https://battlearena:tobattle!@web-stable.arenum.games/ru/')
+        Login = driver.find_element_by_xpath('//button[contains(text(),Войти)]')
+        Login.click()
+        Login_wait = WebDriverWait(driver, 10).until(EC.presence_of_element_located(
+            (By.XPATH, '//*[@id="__layout"]/div/div[4]/div[2]/div/div[2]/div[2]/button[2]')))
+        driver.execute_script("arguments[0].click();", Login_wait)
+        Login_Social = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.NAME, 'email')))
+        driver.find_element_by_name('email').send_keys('p_a.a.skvortsov@mpt.ru')
+        driver.find_element_by_name('password').send_keys('Bass2000v_8')
+        driver.find_element_by_xpath('//*[@id="app-mount"]/div[2]/div/div[2]/div/div/form/div/div/div[1]/div[3]/button[2]').click()
+        Login_Social = WebDriverWait(driver, 10).until(EC.presence_of_element_located(
+            (By.XPATH, '//*[@id="app-mount"]/div[2]/div/div[2]/div/div/div[2]/button[2]')))
+        driver.find_element_by_xpath('//*[@id="app-mount"]/div[2]/div/div[2]/div/div/div[2]/button[2]').click()
+        Login_Succes = WebDriverWait(driver, 100).until(EC.presence_of_element_located(
+            (By.CLASS_NAME, 'header-profile')))
+        self.assertTrue(Login_Succes,'Succes!')
+    def tearDown(self):
+        self.driver.close()
+
+if __name__ == "__main__":
+    unittest.main()

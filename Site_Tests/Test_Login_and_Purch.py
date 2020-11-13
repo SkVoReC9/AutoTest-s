a2158a1420c7b99313105d26a86179e26c248f7e
Your Name <sashaskvorcov19@gmail.com>
2020-11-13 11:35:05 +0300

    Code refactor

--- Site_Tests/Test_Login_and_Purch.py
+++ Site_Tests/Test_Login_and_Purch.py
@@ -1,10 +1,8 @@
 from selenium import webdriver
-from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-import datetime
 from selenium.common.exceptions import TimeoutException
 import unittest
 import random
@@ -20,78 +18,83 @@ class TestLogin(unittest.TestCase):
         print('Discord login start')
         driver = self.driver
         driver.get('https://battlearena:tobattle!@web-stable.arenum.games/ru/')
-        Login = driver.find_element_by_xpath('//button[contains(text(),Войти)]')
-        Login.click()
-        Login_wait = WebDriverWait(driver, 10).until(EC.presence_of_element_located(
+        login = driver.find_element_by_xpath('//button[contains(text(),Войти)]')
+        login.click()
+        login_wait = WebDriverWait(driver, 10).until(EC.presence_of_element_located(
             (By.XPATH, '//*[@id="__layout"]/div/div[4]/div[2]/div/div[2]/div[2]/button[2]')))
-        driver.execute_script("arguments[0].click();", Login_wait)
+        driver.execute_script("arguments[0].click();", login_wait)
 
-        Login_Social = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.NAME, 'email')))
+        WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.NAME, 'email')))
         driver.find_element_by_name('email').send_keys('p_a.a.skvortsov@mpt.ru')
         driver.find_element_by_name('password').send_keys('Bass2000v_8')
-        driver.find_element_by_xpath('//*[@id="app-mount"]/div[2]/div/div[2]/div/div/form/div/div/div[1]/div[3]/button[2]').click()
-        Login_Social = WebDriverWait(driver, 10).until(EC.element_to_be_clickable(
+        driver.find_element_by_xpath(
+            '//*[@id="app-mount"]/div[2]/div/div[2]/div/div/form/div/div/div[1]/div[3]/button[2]').click()
+        WebDriverWait(driver, 10).until(EC.element_to_be_clickable(
             (By.XPATH, '//*[@id="app-mount"]/div[2]/div/div[2]/div/div/div[2]/button[2]')))
         driver.find_element_by_xpath('//*[@id="app-mount"]/div[2]/div/div[2]/div/div/div[2]/button[2]').click()
         print('Discord Login Ok!')
         print('Start Arenum Quests')
         #Переходим в профиль для выполнения задания
-        Login_Succes = WebDriverWait(driver, 100).until(EC.element_to_be_clickable(
+        WebDriverWait(driver, 100).until(EC.element_to_be_clickable(
             (By.CLASS_NAME, 'header-profile')))
-        LogOut_Dis = driver.find_element_by_class_name('header-profile').click()
+        driver.find_element_by_class_name('header-profile').click()
 
         #Выполение заданий
-        Go_To_Quest = WebDriverWait(driver, 10).until(EC.visibility_of_element_located(
+        WebDriverWait(driver, 10).until(EC.visibility_of_element_located(
             (By.XPATH, '//*[@id="__layout"]/div/div[1]/div/div/div/div[2]/div[2]/div[3]/div/a[1]')))
-        Go_To_Quest = driver.find_element_by_xpath('//*[@id="__layout"]/div/div[1]/div/div/div/div[2]/div[2]/div[3]/div/a[1]').click()
+        driver.find_element_by_xpath('//*[@id="__layout"]/div/div[1]/div/div/div/div[2]/div[2]/div[3]/div/a[1]').click()
         try:
-            Go_To_Quest = WebDriverWait(driver, 10).until(EC.presence_of_element_located(
+            WebDriverWait(driver, 10).until(EC.presence_of_element_located(
                 (By.XPATH, '//*[@id="__layout"]/div/main/div/div[2]/div[2]/div[2]/div[2]/div[1]/div[2]')))
-            Go_To_Quest = driver.find_element_by_xpath('//*[@id="__layout"]/div/main/div/div[2]/div[2]/div[2]/div[2]/div[1]/div[2]').click()
+            driver.find_element_by_xpath(
+                '//*[@id="__layout"]/div/main/div/div[2]/div[2]/div[2]/div[2]/div[1]/div[2]').click()
         except TimeoutException:
-            LogOut_Dis = driver.find_element_by_class_name('header-profile').click()
-            Go_To_Quest = WebDriverWait(driver, 10).until(EC.presence_of_element_located(
+            driver.find_element_by_class_name('header-profile').click()
+            WebDriverWait(driver, 10).until(EC.presence_of_element_located(
                 (By.XPATH, '//*[@id="__layout"]/div/div[1]/div/div/div/div[2]/div[2]/div[3]/div/a[1]')))
-            Go_To_Quest = driver.find_element_by_xpath('//*[@id="__layout"]/div/div[1]/div/div/div/div[2]/div[2]/div[3]/div/a[1]').click()
-            Go_To_Quest = WebDriverWait(driver, 10).until(EC.presence_of_element_located(
+            driver.find_element_by_xpath(
+                '//*[@id="__layout"]/div/div[1]/div/div/div/div[2]/div[2]/div[3]/div/a[1]').click()
+            WebDriverWait(driver, 10).until(EC.presence_of_element_located(
                 (By.XPATH, '//*[@id="__layout"]/div/main/div/div[2]/div[2]/div[2]/div[2]/div[1]/div[2]')))
-            Go_To_Quest = driver.find_element_by_xpath(
+            driver.find_element_by_xpath(
                 '//*[@id="__layout"]/div/main/div/div[2]/div[2]/div[2]/div[2]/div[1]/div[2]').click()
         #Переходим во вкладку задания
-        Go_To_Quest = driver.find_element_by_xpath('//*[@id="__layout"]/div/main/div/div[2]/div[2]/div[2]/div[2]/div[1]/div[2]/div').click()
-        Go_To_Quest = WebDriverWait(driver, 10).until(EC.presence_of_element_located(
+        driver.find_element_by_xpath(
+            '//*[@id="__layout"]/div/main/div/div[2]/div[2]/div[2]/div[2]/div[1]/div[2]/div').click()
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located(
             (By.XPATH, '//*[@id="profile-rewards"]/div[1]/a[2]')))
         #Переходим на задания по заполнению мобильного телефона
-        Go_To_Quest = driver.find_element_by_xpath('//*[@id="profile-rewards"]/div[1]/a[2]').click()
-        Quest_Telephone = WebDriverWait(driver, 10).until(EC.presence_of_element_located(
+        driver.find_element_by_xpath('//*[@id="profile-rewards"]/div[1]/a[2]').click()
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located(
             (By.XPATH, '//input[@name="telephone"]')))
         a = '+790000'+str(random.randint(10000, 99999))
         print(a)
-        Quest_Telephone = driver.find_element_by_xpath('//input[@name="telephone"]').send_keys(a)
-        Quest_Telephone = driver.find_element_by_xpath('//*[@id="__layout"]/div/main/div/div/div[3]/div/button[1]').click()
-        Quest_Telephone = WebDriverWait(driver, 10).until(EC.visibility_of_element_located(
+        driver.find_element_by_xpath('//input[@name="telephone"]').send_keys(a)
+        driver.find_element_by_xpath('//*[@id="__layout"]/div/main/div/div/div[3]/div/button[1]').click()
+        WebDriverWait(driver, 10).until(EC.visibility_of_element_located(
             (By.XPATH, '//*[@id="__layout"]/div/div[6]/div[2]/div/div[1]')))
-        Quest_Telephone = driver.find_element_by_xpath('//*[@id="__layout"]/div/div[6]/div[2]/div/div[1]').click()
+        driver.find_element_by_xpath('//*[@id="__layout"]/div/div[6]/div[2]/div/div[1]').click()
 
         print('Arenum Quests OK!')
 
         # Выходим из аккаунта после выполнения заданий
-        LogOut_Dis = driver.find_element_by_class_name('header-profile').click()
-        LogOut_Dis = WebDriverWait(driver,10).LogOut_Dis = WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.XPATH, '//*[@id="__layout"]/div/div[1]/div/div/div/div[2]/div[2]/div[3]/div/a[2]')))
-        LogOut_Dis = driver.find_element_by_xpath('//*[@id="__layout"]/div/div[1]/div/div/div/div[2]/div[2]/div[3]/div/a[2]').click()
-        LogOut_Dis = WebDriverWait(driver, 10).until(
+        driver.find_element_by_class_name('header-profile').click()
+        WebDriverWait(driver,10).LogOut_Dis = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located(
+                (By.XPATH, '//*[@id="__layout"]/div/div[1]/div/div/div/div[2]/div[2]/div[3]/div/a[2]')))
+        driver.find_element_by_xpath('//*[@id="__layout"]/div/div[1]/div/div/div/div[2]/div[2]/div[3]/div/a[2]').click()
+        WebDriverWait(driver, 10).until(
             EC.presence_of_element_located((By.XPATH, '//*[@id="__layout"]/div/main/div[1]/div[2]/div/div[6]/span')))
 
-        LogOut_Dis = driver.find_element_by_xpath('//*[@id="__layout"]/div/main/div[1]/div[2]/div/div[6]/span').click()
-        LogOut_Dis = WebDriverWait(driver, 10).until(
+        driver.find_element_by_xpath('//*[@id="__layout"]/div/main/div[1]/div[2]/div/div[6]/span').click()
+        WebDriverWait(driver, 10).until(
             EC.presence_of_element_located((By.XPATH, '//*[@id="__layout"]/div/div[3]/div[2]/div/div[2]/div[2]/a[1]')))
-        LogOut_Dis = driver.find_element_by_xpath('//*[@id="__layout"]/div/div[3]/div[2]/div/div[2]/div[2]/a[1]').click()
-        LogOut = driver.find_element_by_xpath('//button[contains(text(),Войти)]')
-        self.assertTrue(LogOut, 'Test Failed')
+        driver.find_element_by_xpath('//*[@id="__layout"]/div/div[3]/div[2]/div/div[2]/div[2]/a[1]').click()
+        log_out = driver.find_element_by_xpath('//button[contains(text(),Войти)]')
+        self.assertTrue(log_out, 'Test Failed')
         print('Discord log out ok!')
         print('Discord Test OK!')
-
+    #@unittest.skip('Google test skipped for FB')
     def test_log_in_Google(self):
         print('Google test start')
         print('Google login start')
@@ -102,46 +105,110 @@ class TestLogin(unittest.TestCase):
         Login_wait = WebDriverWait(driver, 10).until(EC.presence_of_element_located(
             (By.XPATH, '//*[@id="__layout"]/div/div[4]/div[2]/div/div[2]/div[2]/button[3]')))
         driver.execute_script("arguments[0].click();", Login_wait)
-        Login_Social = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.NAME, 'identifier')))
-        Login_Social = driver.find_element_by_name('identifier').send_keys('skvorcov_a@arenum.games')
-        Login_Social = driver.find_element_by_xpath('//*[@id="identifierNext"]/div/button/div[2]').click()
-        Login_Social = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.NAME, 'password')))
-        Login_Social = driver.find_element_by_name('password').send_keys('Bass2000v_8f_8trestat')
-        Login_Social = driver.find_element_by_xpath('//*[@id="passwordNext"]/div/button/div[2]').click()
+        WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.NAME, 'identifier')))
+        driver.find_element_by_name('identifier').send_keys('skvorcov_a@arenum.games')
+        driver.find_element_by_xpath('//*[@id="identifierNext"]/div/button/div[2]').click()
+        WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.NAME, 'password')))
+        driver.find_element_by_name('password').send_keys('Bass2000v_8f_8trestat')
+        driver.find_element_by_xpath('//*[@id="passwordNext"]/div/button/div[2]').click()
         print('Google login OK!')
         #Переходим в профиль
         print('Start Arenum quests')
-        Login_Social = WebDriverWait(driver, 100).until(EC.presence_of_element_located(
+        WebDriverWait(driver, 100).until(EC.presence_of_element_located(
             (By.CLASS_NAME, 'header-profile')))
-        Login_Social = driver.find_element_by_class_name('header-profile').click()
-        Go_To_Quest = WebDriverWait(driver, 10).until(EC.visibility_of_element_located(
+        driver.find_element_by_class_name('header-profile').click()
+        WebDriverWait(driver, 10).until(EC.visibility_of_element_located(
             (By.XPATH, '//*[@id="__layout"]/div/div[1]/div/div/div/div[2]/div[2]/div[3]/div/a[1]')))
-        Go_To_Quest = driver.find_element_by_xpath(
+        driver.find_element_by_xpath(
             '//*[@id="__layout"]/div/div[1]/div/div/div/div[2]/div[2]/div[3]/div/a[1]').click()
         try:
-            Go_To_Quest = WebDriverWait(driver, 10).until(EC.presence_of_element_located(
+            WebDriverWait(driver, 10).until(EC.presence_of_element_located(
                 (By.XPATH, '//*[@id="__layout"]/div/main/div/div[2]/div[2]/div[2]/div[2]/div[1]/div[2]')))
-            Go_To_Quest = driver.find_element_by_xpath(
+            driver.find_element_by_xpath(
                 '//*[@id="__layout"]/div/main/div/div[2]/div[2]/div[2]/div[2]/div[1]/div[2]').click()
         except TimeoutException:
-            LogOut_Dis = driver.find_element_by_class_name('header-profile').click()
-            Go_To_Quest = WebDriverWait(driver, 10).until(EC.presence_of_element_located(
+            driver.find_element_by_class_name('header-profile').click()
+            WebDriverWait(driver, 10).until(EC.presence_of_element_located(
                 (By.XPATH, '//*[@id="__layout"]/div/div[1]/div/div/div/div[2]/div[2]/div[3]/div/a[1]')))
-            Go_To_Quest = driver.find_element_by_xpath(
+            driver.find_element_by_xpath(
                 '//*[@id="__layout"]/div/div[1]/div/div/div/div[2]/div[2]/div[3]/div/a[1]').click()
-            Go_To_Quest = WebDriverWait(driver, 10).until(EC.presence_of_element_located(
+            WebDriverWait(driver, 10).until(EC.presence_of_element_located(
                 (By.XPATH, '//*[@id="__layout"]/div/main/div/div[2]/div[2]/div[2]/div[2]/div[1]/div[2]')))
-            Go_To_Quest = driver.find_element_by_xpath(
+            driver.find_element_by_xpath(
                 '//*[@id="__layout"]/div/main/div/div[2]/div[2]/div[2]/div[2]/div[1]/div[2]').click()
         #Переходим во вкладку задания
-        Go_To_Quest = driver.find_element_by_xpath(
+        driver.find_element_by_xpath(
             '//*[@id="__layout"]/div/main/div/div[2]/div[2]/div[2]/div[2]/div[1]/div[2]/div').click()
-        Go_To_Quest = WebDriverWait(driver, 10).until(EC.presence_of_element_located(
+        go_to_quest = WebDriverWait(driver, 10).until(EC.presence_of_element_located(
             (By.XPATH, '//*[@id="profile-rewards"]/div[1]/a[2]')))
 
-        self.assertTrue(Go_To_Quest, "Test Failed")
+        self.assertTrue(go_to_quest, "Test Failed")
         print('Google Arenum Quest OK!')
         print('Google test OK!')
+    #@unittest.skip('Skipped for AppleId')
+    def test_log_in_Facebook(self):
+        print('Facebook test start')
+        print('Facebook login start')
+        driver = self.driver
+        driver.get('https://battlearena:tobattle!@web-stable.arenum.games/ru/')
+        driver.find_element_by_xpath('//button[contains(text(),Войти)]').click()
+        # Вход на сайт через Google аккаунт
+        Login_wait = WebDriverWait(driver, 10).until(EC.presence_of_element_located(
+            (By.XPATH, '//*[@id="__layout"]/div/div[4]/div[2]/div/div[2]/div[2]/button[4]')))
+        driver.execute_script("arguments[0].click();", Login_wait)
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located(
+            (By.NAME, 'email')))
+        driver.find_element_by_name('email').send_keys('sashaskvorcov19@gmail.com')
+        driver.find_element_by_name('pass').send_keys('Bass2000v_8f_8trestat')
+        driver.find_element_by_name('login').click()
+        login_social = WebDriverWait(driver, 100).until(EC.presence_of_element_located(
+            (By.CLASS_NAME, 'header-profile')))
+        self.assertTrue(login_social, 'Test Failed')
+        print('Facebook login OK!')
+        print('Facebook test OK!')
+    @unittest.skip('Do not work with 2 step auth')
+    def test_log_in_AppleId(self):
+        print('AppleId test start')
+        print('AppleId login start')
+        driver = self.driver
+        driver.get('https://battlearena:tobattle!@web-stable.arenum.games/ru/')
+        driver.find_element_by_xpath('//button[contains(text(),Войти)]').click()
+        # Вход на сайт через AppleID
+        login_wait = WebDriverWait(driver, 10).until(EC.presence_of_element_located(
+            (By.XPATH, '//*[@id="__layout"]/div/div[4]/div[2]/div/div[2]/div[2]/button[5]')))
+        driver.execute_script("arguments[0].click();", login_wait)
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located(
+            (By.ID, 'account_name_text_field')))
+        driver.find_element_by_id('account_name_text_field').send_keys('skvorcov_a@arenum.games')
+        driver.find_element_by_id('account_name_text_field').send_keys(Keys.ENTER)
+        WebDriverWait(driver, 10).until(EC.visibility_of_element_located(
+            (By.ID, 'password_text_field')
+        ))
+        driver.find_element_by_id('password_text_field').send_keys('pass')
+        driver.find_element_by_id('password_text_field').send_keys(Keys.ENTER)
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located(
+            (By.XPATH, '//*[@id="1605251974871-0"]/div/div/button[1]')))
+        driver.find_element_by_xpath('//*[@id="1605251974871-0"]/div/div/button[1]').click()
+        #Выбор региона
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located(
+            (By.XPATH, '//*[@id="__layout"]/div/div[3]/div[2]/div/div/div[2]/button[2]')))
+        driver.find_element_by_xpath('//*[@id="__layout"]/div/div[3]/div[2]/div/div/div[2]/button[2]').click()
+        #Повторный логин
+        driver.find_element_by_id('account_name_text_field').send_keys('skvorcov_a@arenum.games')
+        driver.find_element_by_id('account_name_text_field').send_keys(Keys.ENTER)
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located(
+            (By.ID, 'password_text_field')
+        ))
+        driver.find_element_by_id('password_text_field').send_keys('pass')
+        driver.find_element_by_id('password_text_field').send_keys(Keys.ENTER)
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located(
+            (By.XPATH, '//*[@id="1605251974871-0"]/div/div/button[1]')))
+        driver.find_element_by_xpath('//*[@id="1605251974871-0"]/div/div/button[1]').click()
+        #Профиль
+        login_Social = WebDriverWait(driver, 100).until(EC.presence_of_element_located(
+            (By.CLASS_NAME, 'header-profile')))
+        self.assertTrue(login_Social, 'Test Failed')
+
     def tearDown(self):
         self.driver.close()
 

fcb79b5951b7515034a658e6b324d4fafd315335
Your Name <sashaskvorcov19@gmail.com>
2020-11-14 16:43:57 +0300

    Added PyTest test's and Allure reports

--- Site_Tests/Test_Login_Social.py
+++ Site_Tests/Test_Login_Social.py
@@ -9,9 +9,9 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 
 
-class Test_Login:
+class Test_Login_Google:
 
-    @pytest.fixture(scope="class")
+    @pytest.fixture(scope="class", autouse=True)
     def driver(self):
         print('Initialize driver')
         driver = webdriver.Chrome(executable_path='C:/Users/SkVoReC/Desktop/Работа/Автотесты/chromedriver.exe')
@@ -20,20 +20,66 @@ class Test_Login:
         print('Close driver')
         driver.quit()
 
-    def test_google_login(self, driver):
-        driver.find_element_by_xpath('//button[contains(text(),Войти)]').click()
-        # Вход на сайт через Google аккаунт
-        login_wait = WebDriverWait(driver, 10).until(EC.presence_of_element_located(
-            (By.XPATH, '//*[@id="__layout"]/div/div[4]/div[2]/div/div[2]/div[2]/button[3]')))
-        driver.execute_script("arguments[0].click();", login_wait)
-        WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.NAME, 'identifier')))
-        driver.find_element_by_name('identifier').send_keys('skvorcov_a@arenum.games')
-        driver.find_element_by_xpath('//*[@id="identifierNext"]/div/button/div[2]').click()
-        WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.NAME, 'password')))
-        driver.find_element_by_name('password').send_keys('Bass2000v_8f_8trestat')
-        driver.find_element_by_xpath('//*[@id="passwordNext"]/div/button/div[2]').click()
-        result = WebDriverWait(driver, 100).until(EC.visibility_of_element_located(
-            (By.CLASS_NAME, 'header-profile')))
-        assert result == True
-        print('Google login OK!')
+    @pytest.fixture(scope='function')
+    def test_google(self, driver):
+        print('Start test')
+        yield
+        print('End test')
 
+    @allure.feature('Google')
+    @allure.story('Заходим в Google аккаунт')
+    def test_google_login(self, driver, test_google):
+        with allure.step('Нажимаем на войти'):
+            driver.find_element_by_xpath('//button[contains(text(),Войти)]').click()
+        # Вход на сайт через Google аккаунт
+        with allure.step('Выбираем Google'):
+            login_wait = WebDriverWait(driver, 10).until(EC.presence_of_element_located(
+                (By.XPATH, '//*[@id="__layout"]/div/div[4]/div[2]/div/div[2]/div[2]/button[3]')))
+            driver.execute_script("arguments[0].click();", login_wait)
+        with allure.step('Вводим данные и заходим'):
+            WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.NAME, 'identifier')))
+            driver.find_element_by_name('identifier').send_keys('skvorcov_a@arenum.games')
+            driver.find_element_by_xpath('//*[@id="identifierNext"]/div/button/div[2]').click()
+            WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.NAME, 'password')))
+            driver.find_element_by_name('password').send_keys('Bass2000v_8f_8trestat')
+            driver.find_element_by_xpath('//*[@id="passwordNext"]/div/button/div[2]').click()
+        try:
+            WebDriverWait(driver, 100).until(EC.visibility_of_element_located(
+                (By.CLASS_NAME, 'header-profile')))
+            assert True
+        except TimeoutException:
+            assert False
+
+    @allure.feature('Google')
+    @allure.story('Заходим в Google аккаунт')
+    def test_quest_google(self, driver, test_google):
+        with allure.step('Переходим в профиль'):
+            WebDriverWait(driver, 100).until(EC.visibility_of_element_located(
+                (By.CLASS_NAME, 'header-profile')))
+            driver.find_element_by_class_name('header-profile').click()
+            WebDriverWait(driver, 10).until(EC.visibility_of_element_located(
+                (By.XPATH, '//*[@id="__layout"]/div/div[1]/div/div/div/div[2]/div[2]/div[3]/div/a[1]')))
+            driver.find_element_by_xpath(
+                '//*[@id="__layout"]/div/div[1]/div/div/div/div[2]/div[2]/div[3]/div/a[1]').click()
+            try:
+                WebDriverWait(driver, 10).until(EC.presence_of_element_located(
+                    (By.XPATH, '//*[@id="__layout"]/div/main/div/div[2]/div[2]/div[2]/div[2]/div[1]/div[2]')))
+                driver.find_element_by_xpath(
+                    '//*[@id="__layout"]/div/main/div/div[2]/div[2]/div[2]/div[2]/div[1]/div[2]').click()
+            except TimeoutException:
+                driver.find_element_by_class_name('header-profile').click()
+                WebDriverWait(driver, 10).until(EC.presence_of_element_located(
+                    (By.XPATH, '//*[@id="__layout"]/div/div[1]/div/div/div/div[2]/div[2]/div[3]/div/a[1]')))
+                driver.find_element_by_xpath(
+                    '//*[@id="__layout"]/div/div[1]/div/div/div/div[2]/div[2]/div[3]/div/a[1]').click()
+                WebDriverWait(driver, 10).until(EC.presence_of_element_located(
+                    (By.XPATH, '//*[@id="__layout"]/div/main/div/div[2]/div[2]/div[2]/div[2]/div[1]/div[2]')))
+                driver.find_element_by_xpath(
+                    '//*[@id="__layout"]/div/main/div/div[2]/div[2]/div[2]/div[2]/div[1]/div[2]').click()
+        with allure.step('Переходим во вкладку задания'):
+        # Переходим во вкладку задания
+            driver.find_element_by_xpath(
+                '//*[@id="__layout"]/div/main/div/div[2]/div[2]/div[2]/div[2]/div[1]/div[2]/div').click()
+            go_to_quest = WebDriverWait(driver, 10).until(EC.presence_of_element_located(
+                (By.XPATH, '//*[@id="profile-rewards"]/div[1]/a[2]')))
+        assert True

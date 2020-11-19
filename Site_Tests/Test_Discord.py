3ae1fd3afae5e877e38ec5826315cea1b84b6bd7
Your Name <sashaskvorcov19@gmail.com>
2020-11-19 23:25:32 +0300

    Refactor for too fast time

--- Site_Tests/Test_Discord.py
+++ Site_Tests/Test_Discord.py
@@ -19,7 +19,7 @@ def pytest_generate_tests(metafunc):
 
 
 @allure.epic("Тестирование с помощью Discord")
-@allure.feature('Вход и выполнение задания')
+@allure.feature('Вход, выполнение задания, смена аватарки, никнейма')
 class TestDiscord:
     @pytest.fixture(scope="class", autouse=True)
     def driver(self, url):
@@ -31,7 +31,7 @@ class TestDiscord:
         driver.close()
 
     @allure.epic("Тестирование с помощью Discord")
-    @allure.feature('Вход и выполнение задания')
+    @allure.feature('Вход, выполнение задания, смена аватарки, никнейма')
     @allure.story('Заходим в Discord аккаунт')
     def test_discord_login(self, driver):
         with allure.step('Открываем сайт  и нажимаем войти'):
@@ -58,7 +58,7 @@ class TestDiscord:
             assert False
 
     @allure.epic("Тестирование с помощью Discord")
-    @allure.feature('Вход и выполнение задания')
+    @allure.feature('Вход, выполнение задания, смена аватарки, никнейма')
     @allure.story('Выполняем задания')
     def test_quest_discord(self, driver):
         with allure.step('Переходим в профиль'):
@@ -66,24 +66,12 @@ class TestDiscord:
                 (By.XPATH, '//*[@id="__layout"]/div/div[1]/div/div/div/div[2]/div[2]/div[2]/picture')))
             driver.find_element_by_xpath(
                 '//*[@id="__layout"]/div/div[1]/div/div/div/div[2]/div[2]/div[2]/picture').click()
-            time.sleep(1)
+            time.sleep(2.5)
             WebDriverWait(driver, 5).until(ec.presence_of_element_located(
                 (By.XPATH, '//*[@id="__layout"]/div/div[1]/div/div/div/div[2]/div[2]/div[3]/div/a[1]')))
             driver.find_element_by_xpath(
                 '//*[@id="__layout"]/div/div[1]/div/div/div/div[2]/div[2]/div[3]/div/a[1]').click()
-        try:
             WebDriverWait(driver, 4).until(ec.presence_of_element_located(
-                (By.XPATH, '//*[@id="__layout"]/div/main/div/div[2]/div[2]/div[2]/div[2]/div[1]/div[2]')))
-            driver.find_element_by_xpath(
-                '//*[@id="__layout"]/div/main/div/div[2]/div[2]/div[2]/div[2]/div[1]/div[2]').click()
-        except TimeoutException:
-            driver.find_element_by_xpath(
-                '//*[@id="__layout"]/div/div[1]/div/div/div/div[2]/div[2]/div[2]/picture').click()
-            WebDriverWait(driver, 4).until(ec.presence_of_element_located(
-                (By.XPATH, '//*[@id="__layout"]/div/div[1]/div/div/div/div[2]/div[2]/div[3]/div/a[1]')))
-            driver.find_element_by_xpath(
-                '//*[@id="__layout"]/div/div[1]/div/div/div/div[2]/div[2]/div[3]/div/a[1]').click()
-            WebDriverWait(driver, 10).until(ec.presence_of_element_located(
                 (By.XPATH, '//*[@id="__layout"]/div/main/div/div[2]/div[2]/div[2]/div[2]/div[1]/div[2]')))
             driver.find_element_by_xpath(
                 '//*[@id="__layout"]/div/main/div/div[2]/div[2]/div[2]/div[2]/div[1]/div[2]').click()
@@ -94,42 +82,56 @@ class TestDiscord:
         go_to_quest = WebDriverWait(driver, 10).until(ec.presence_of_element_located(
             (By.XPATH, '//*[@id="profile-rewards"]/div[1]/a[2]')))
         assert True
+
+    @allure.epic("Тестирование с помощью Discord")
+    @allure.feature('Вход, выполнение задания, смена аватарки, никнейма')
+    @allure.story('Меняем аватарку')
     def test_avatar_discord(self, driver):
-        driver.find_element_by_xpath(
-            '//*[@id="__layout"]/div/main/div/div[2]/div[2]/div[2]/div[1]/div/div[2]/button[2]').click()
-        WebDriverWait(driver, 5).until(ec.presence_of_element_located(
-            (By.XPATH, '//*[@id="__layout"]/div/main/div[1]/div[2]/div[2]/div[2]/div[2]')))
+        with allure.step('Переходим во вкладку редактирования'):
+            driver.find_element_by_xpath(
+                '//*[@id="__layout"]/div/main/div/div[2]/div[2]/div[2]/div[1]/div/div[2]/button[2]').click()
+            WebDriverWait(driver, 5).until(ec.presence_of_element_located(
+                (By.XPATH, '//*[@id="__layout"]/div/main/div[1]/div[2]/div[2]/div[2]/div[2]')))
         WebDriverWait(driver, 5).until(ec.presence_of_element_located(
             (By.XPATH, '//*[@id="__layout"]/div/main/div[1]/div[2]/div[1]/div[1]/input')
         ))
-        driver.find_element_by_xpath('//*[@id="__layout"]/div/main/div[1]/div[2]/div[1]/div[1]/input').send_keys(
-            'C:/Users/SkVoReC/Desktop/Работа/Автотесты/Picturies/Arenum_' + str(random.randint(1, 3)) + '.jpg')
-        time.sleep(5)
-        driver.find_element_by_xpath('//*[@id="__layout"]/div/main/div[1]/div[1]/div/a').click()
+        with allure.step('Загружаем одну из 3 картинок'):
+            driver.find_element_by_xpath('//*[@id="__layout"]/div/main/div[1]/div[2]/div[1]/div[1]/input').send_keys(
+                'C:/Users/SkVoReC/Desktop/Работа/Автотесты/Picturies/Arenum_' + str(random.randint(1, 3)) + '.jpg')
+        time.sleep(3)
+        with allure.step('Выходим обратно в профиль'):
+            driver.find_element_by_xpath('//*[@id="__layout"]/div/main/div[1]/div[1]/div/a').click()
         WebDriverWait(driver, 100).until(ec.visibility_of_element_located(
             (By.CLASS_NAME, 'header-profile')))
         assert True
 
+    @allure.epic("Тестирование с помощью Discord")
+    @allure.feature('Вход, выполнение задания, смена аватарки, никнейма')
+    @allure.story('Меняем никнейм')
     def test_nickname_discord(self, driver):
-        WebDriverWait(driver, 5).until(ec.presence_of_element_located(
-            (By.XPATH, '//*[@id="__layout"]/div/main/div/div[2]/div[2]/div[2]/div[1]/div/div[2]/button[2]')
-        ))
-        driver.find_element_by_xpath(
-            '//*[@id="__layout"]/div/main/div/div[2]/div[2]/div[2]/div[1]/div/div[2]/button[2]').click()
+        with allure.step('Переходим во вкладку редактирования'):
+            WebDriverWait(driver, 5).until(ec.presence_of_element_located(
+                (By.XPATH, '//*[@id="__layout"]/div/main/div/div[2]/div[2]/div[2]/div[1]/div/div[2]/button[2]')
+            ))
+            driver.find_element_by_xpath(
+                '//*[@id="__layout"]/div/main/div/div[2]/div[2]/div[2]/div[1]/div/div[2]/button[2]').click()
         WebDriverWait(driver, 5).until(ec.visibility_of_element_located(
             (By.XPATH, '//*[@id="__layout"]/div/main/div[1]/div[2]/div[2]/div[2]/div[2]')))
         time.sleep(1)
-        driver.find_element_by_xpath('//*[@id="__layout"]/div/main/div[1]/div[2]/div[2]/div[2]/div[2]').click()
+        with allure.step('Нажимаем на никнейм'):
+            driver.find_element_by_xpath('//*[@id="__layout"]/div/main/div[1]/div[2]/div[2]/div[2]/div[2]').click()
         WebDriverWait(driver, 5).until(ec.visibility_of_element_located(
             (By.XPATH, '//*[@id="__layout"]/div/main/div[1]/div[2]/div/div/div[2]')
         ))
-        driver.execute_script("setTimeout(() => {document.getElementsByClassName('input--big')[0].focus()}, 1000)")
+        with allure.step('Очищаем поле и меняем никнейм'):
+            driver.execute_script("setTimeout(() => {document.getElementsByClassName('input--big')[0].focus()}, 1000)")
         action = action_chains.ActionChains(driver)
         action.move_by_offset(907, 457).click().perform()
         time.sleep(1)
         driver.find_element_by_class_name('input--big').clear()
         driver.find_element_by_class_name('input--big').send_keys('Discord_'+str(random.randint(1, 50)))
-        driver.find_element_by_xpath('//*[@id="__layout"]/div/main/div[1]/div[1]/div/div[2]').click()
+        with allure.step('Сохраняем и возвращаемся в профиль'):
+            driver.find_element_by_xpath('//*[@id="__layout"]/div/main/div[1]/div[1]/div/div[2]').click()
         WebDriverWait(driver, 100).until(ec.visibility_of_element_located(
             (By.XPATH, '//*[@id="__layout"]/div/main/div[2]/div/a[2]')))
         time.sleep(1)
--- Site_Tests/Test_Google.py
+++ Site_Tests/Test_Google.py
@@ -19,7 +19,7 @@ def pytest_generate_tests(metafunc):
 
 
 @allure.epic("Тестирование с помощью Google")
-@allure.feature('Вход и выполнение задания')
+@allure.feature('Вход, выполнение задания, смена аватарки, никнейма')
 class TestGoogle:
     @pytest.yield_fixture(scope="class", autouse=True)
     def driver(self, url):
@@ -31,7 +31,7 @@ class TestGoogle:
         driver.close()
 
     @allure.epic("Тестирование с помощью Google")
-    @allure.feature('Вход и выполнение задания')
+    @allure.feature('Вход, выполнение задания, смена аватарки, никнейма')
     @allure.story('Заходим в Google аккаунт')
     def test_google_login(self, driver):
         with allure.step('Открываем сайт и нажимаем войти'):
@@ -56,7 +56,7 @@ class TestGoogle:
             assert False
 
     @allure.epic("Тестирование с помощью Google")
-    @allure.feature('Вход и выполнение задания')
+    @allure.feature('Вход, выполнение задания, смена аватарки, никнейма')
     @allure.story('Выполняем задания')
     def test_quest_google(self, driver):
         with allure.step('Переходим в профиль'):
@@ -64,24 +64,12 @@ class TestGoogle:
                 (By.XPATH, '//*[@id="__layout"]/div/div[1]/div/div/div/div[2]/div[2]/div[2]/picture')))
             driver.find_element_by_xpath(
                 '//*[@id="__layout"]/div/div[1]/div/div/div/div[2]/div[2]/div[2]/picture').click()
-            time.sleep(1)
+            time.sleep(2.5)
             WebDriverWait(driver, 5).until(ec.presence_of_element_located(
                 (By.XPATH, '//*[@id="__layout"]/div/div[1]/div/div/div/div[2]/div[2]/div[3]/div/a[1]')))
             driver.find_element_by_xpath(
                 '//*[@id="__layout"]/div/div[1]/div/div/div/div[2]/div[2]/div[3]/div/a[1]').click()
-        try:
             WebDriverWait(driver, 4).until(ec.presence_of_element_located(
-                (By.XPATH, '//*[@id="__layout"]/div/main/div/div[2]/div[2]/div[2]/div[2]/div[1]/div[2]')))
-            driver.find_element_by_xpath(
-                '//*[@id="__layout"]/div/main/div/div[2]/div[2]/div[2]/div[2]/div[1]/div[2]').click()
-        except TimeoutException:
-            driver.find_element_by_xpath(
-                '//*[@id="__layout"]/div/div[1]/div/div/div/div[2]/div[2]/div[2]/picture').click()
-            WebDriverWait(driver, 4).until(ec.presence_of_element_located(
-                (By.XPATH, '//*[@id="__layout"]/div/div[1]/div/div/div/div[2]/div[2]/div[3]/div/a[1]')))
-            driver.find_element_by_xpath(
-                '//*[@id="__layout"]/div/div[1]/div/div/div/div[2]/div[2]/div[3]/div/a[1]').click()
-            WebDriverWait(driver, 10).until(ec.presence_of_element_located(
                 (By.XPATH, '//*[@id="__layout"]/div/main/div/div[2]/div[2]/div[2]/div[2]/div[1]/div[2]')))
             driver.find_element_by_xpath(
                 '//*[@id="__layout"]/div/main/div/div[2]/div[2]/div[2]/div[2]/div[1]/div[2]').click()
@@ -93,42 +81,55 @@ class TestGoogle:
             (By.XPATH, '//*[@id="profile-rewards"]/div[1]/a[2]')))
         assert True
 
+    @allure.epic("Тестирование с помощью Google")
+    @allure.feature('Вход, выполнение задания, смена аватарки, никнейма')
+    @allure.story('Меняем аватарку')
     def test_avatar_google(self, driver):
-        driver.find_element_by_xpath(
-            '//*[@id="__layout"]/div/main/div/div[2]/div[2]/div[2]/div[1]/div/div[2]/button[2]').click()
-        WebDriverWait(driver, 5).until(ec.presence_of_element_located(
-            (By.XPATH, '//*[@id="__layout"]/div/main/div[1]/div[2]/div[2]/div[2]/div[2]')))
+        with allure.step('Переходим во вкладку редактирования'):
+            driver.find_element_by_xpath(
+                '//*[@id="__layout"]/div/main/div/div[2]/div[2]/div[2]/div[1]/div/div[2]/button[2]').click()
+            WebDriverWait(driver, 5).until(ec.presence_of_element_located(
+                (By.XPATH, '//*[@id="__layout"]/div/main/div[1]/div[2]/div[2]/div[2]/div[2]')))
         WebDriverWait(driver, 5).until(ec.presence_of_element_located(
             (By.XPATH, '//*[@id="__layout"]/div/main/div[1]/div[2]/div[1]/div[1]/input')
         ))
-        driver.find_element_by_xpath('//*[@id="__layout"]/div/main/div[1]/div[2]/div[1]/div[1]/input').send_keys(
-            'C:/Users/SkVoReC/Desktop/Работа/Автотесты/Picturies/Arenum_' + str(random.randint(1, 3)) + '.jpg')
-        time.sleep(5)
-        driver.find_element_by_xpath('//*[@id="__layout"]/div/main/div[1]/div[1]/div/a').click()
+        with allure.step('Загружаем одну из 3 картинок'):
+            driver.find_element_by_xpath('//*[@id="__layout"]/div/main/div[1]/div[2]/div[1]/div[1]/input').send_keys(
+                'C:/Users/SkVoReC/Desktop/Работа/Автотесты/Picturies/Arenum_' + str(random.randint(1, 3)) + '.jpg')
+        time.sleep(3)
+        with allure.step('Выходим обратно в профиль'):
+            driver.find_element_by_xpath('//*[@id="__layout"]/div/main/div[1]/div[1]/div/a').click()
         WebDriverWait(driver, 100).until(ec.visibility_of_element_located(
             (By.CLASS_NAME, 'header-profile')))
         assert True
 
+    @allure.epic("Тестирование с помощью Google")
+    @allure.feature('Вход, выполнение задания, смена аватарки, никнейма')
+    @allure.story('Меняем никнейм')
     def test_nickname_google(self, driver):
-        WebDriverWait(driver, 5).until(ec.presence_of_element_located(
-            (By.XPATH, '//*[@id="__layout"]/div/main/div/div[2]/div[2]/div[2]/div[1]/div/div[2]/button[2]')
-        ))
-        driver.find_element_by_xpath(
-            '//*[@id="__layout"]/div/main/div/div[2]/div[2]/div[2]/div[1]/div/div[2]/button[2]').click()
+        with allure.step('Переходим во вкладку редактирования'):
+            WebDriverWait(driver, 5).until(ec.presence_of_element_located(
+                (By.XPATH, '//*[@id="__layout"]/div/main/div/div[2]/div[2]/div[2]/div[1]/div/div[2]/button[2]')
+            ))
+            driver.find_element_by_xpath(
+                '//*[@id="__layout"]/div/main/div/div[2]/div[2]/div[2]/div[1]/div/div[2]/button[2]').click()
         WebDriverWait(driver, 5).until(ec.visibility_of_element_located(
             (By.XPATH, '//*[@id="__layout"]/div/main/div[1]/div[2]/div[2]/div[2]/div[2]')))
         time.sleep(1)
-        driver.find_element_by_xpath('//*[@id="__layout"]/div/main/div[1]/div[2]/div[2]/div[2]/div[2]').click()
+        with allure.step('Нажимаем на никнейм'):
+            driver.find_element_by_xpath('//*[@id="__layout"]/div/main/div[1]/div[2]/div[2]/div[2]/div[2]').click()
         WebDriverWait(driver, 5).until(ec.visibility_of_element_located(
             (By.XPATH, '//*[@id="__layout"]/div/main/div[1]/div[2]/div/div/div[2]')
         ))
-        driver.execute_script("setTimeout(() => {document.getElementsByClassName('input--big')[0].focus()}, 1000)")
+        with allure.step('Очищаем поле и меняем никнейм'):
+            driver.execute_script("setTimeout(() => {document.getElementsByClassName('input--big')[0].focus()}, 1000)")
         action = action_chains.ActionChains(driver)
         action.move_by_offset(907, 457).click().perform()
         time.sleep(1)
         driver.find_element_by_class_name('input--big').clear()
         driver.find_element_by_class_name('input--big').send_keys('Google_'+str(random.randint(1, 50)))
-        driver.find_element_by_xpath('//*[@id="__layout"]/div/main/div[1]/div[1]/div/div[2]').click()
+        with allure.step('Сохраняем и возвращаемся в профиль'):
+            driver.find_element_by_xpath('//*[@id="__layout"]/div/main/div[1]/div[1]/div/div[2]').click()
         WebDriverWait(driver, 100).until(ec.visibility_of_element_located(
             (By.XPATH, '//*[@id="__layout"]/div/main/div[2]/div/a[2]')))
         driver.find_element_by_xpath('//*[@id="__layout"]/div/main/div[2]/div/a[2]').click()
